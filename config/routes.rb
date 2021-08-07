66038b90a95c8005b12a9c67fd66caed8d0361ba
masaki <homu51129@gmail.com>
2021-08-07 12:07:45 +0000

    ログインページへのリンクの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get 'pages/help'
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
