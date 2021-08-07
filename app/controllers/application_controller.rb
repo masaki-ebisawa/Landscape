10dc5ac0f1b88cadddc3ddbcd30c2ff6e08b324f
masaki <homu51129@gmail.com>
2021-08-07 16:32:35 +0000

    application helperとcontrollerの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
   
-  protect_from_forgery with: :exceptio
+  protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+     @current_user ||= User.find_by(id: session[:user_id]) #@current_user = @current_user || User.find_by(id: session[:user_id])と同じ意味
+    end
   end
 
   def logged_in?
